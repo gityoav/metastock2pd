ef20b9bbeee7e7343688c54c1e28b5c3ff036615
gityoav <yoav.git@gmail.com>
2021-11-29 23:54:29 +0000

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import platform
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+setup(name = 'metastock2pd', version = '1.0.0', packages = find_packages(), python_requires = '>=3.7.')
